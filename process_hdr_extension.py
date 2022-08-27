f8958860f3aa6e461c4a6f3382e9f1c284c09cf8
trader0808 <arcmoon12@naver.com>
2022-08-27 23:10:11 +0900

    [ENH] Add rgb conversion for processing hdr merge in right color

--- process_hdr_extension.py
+++ process_hdr_extension.py
@@ -8,10 +8,11 @@ hdr_path = str(Path(current_folder, filename + ".hdr"))
 
 
 img = cv2.imread(hdr_path,flags=cv2.IMREAD_ANYDEPTH)
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 import matplotlib.pyplot as plt
 
-plt.imshow(img)
+plt.imshow(rgb)
 plt.axis('off')
 result_path = Path(current_folder, filename + '.png')
 plt.savefig(result_path)
